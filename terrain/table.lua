a8e3263f4c05810facd4a9071cdf0c2454353b9e
Atom735 <Atom735.ag47@gmail.com>
2021-10-16 18:40:24 +0300

    [TerrainNg] add table

---
+++ terrain/table.lua
@@ -0,0 +1,18 @@
+-- LuaFormatter off
+local TerrainParams = {
+    outer_circle = {
+        radius = 39.0,
+        noise = 10.0,
+    },
+    inner_circle = {
+        radius = 9.5,
+        sand = 7.0,
+    },
+    path_width = 5.0,
+    fish_factor = 0.85,
+    silo_distance = 84,
+    silo_safe_area = 14,
+}
+-- LuaFormatter on
+
+return TerrainParams

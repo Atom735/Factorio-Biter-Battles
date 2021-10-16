abfbe0d9fdb6e7f492f5dd3045954fe29951f6d9
Atom735 <Atom735.ag47@gmail.com>
2021-10-16 20:53:09 +0300

    [TerrainNg] fix spawn ores and add ore cleaner

--- terrain/spawn_ores.lua
+++ terrain/spawn_ores.lua
@@ -1,15 +1,17 @@
 local Noises = require 'utils.noises'
 local TerrainDebug = require 'terrain.debug'
 local DirectionVectors = require 'utils.direction_vectors'
-local Functions = require 'maps.biter_battles_v2.functions'
 
-local spawn_ore = require'maps.biter_battles_v2.tables'.spawn_ore
+local TerrainParams = require 'terrain.table'
 local table_insert = table.insert
 local table_remove = table.remove
 local math_floor = math.floor
-local math_random = math.random
+local math_max = math.max
+local math_min = math.min
 local math_abs = math.abs
 local math_sqrt = math.sqrt
+
+local Public = {}
 
 local function draw_noise_ore_patch(surface, seed, name, position, radius, richness)
 
@@ -37,19 +39,16 @@ local function draw_noise_ore_patch(surface, seed, name, position, radius, richn
 end
 
 
-local function draw_grid_ore_patch(surface, seed, count, grid, name, radius, richness)
+local function draw_grid_ore_patch(surface, seed, random, count, grid, name, radius, richness)
     if not name then return end
     if not radius then return end
     if not richness then return end
-
-    surface = surface or game.surfaces[global.bb_surface_name]
-    seed = seed or surface.map_gen_settings.seed
 
     -- Takes a random left_top coordinate from grid, removes it and draws
     -- ore patch on top of it. Grid is held by reference, so this function
     -- is reentrant.
     for i = 1, count, 1 do
-        local idx = math_floor((math_abs(Noises.random({x = i, y = grid[1].y}, seed)) * (#grid - 0.02)) + 1.01)
+        local idx = math_floor(random(1.1, #grid + 0.9))
         local pos = grid[idx]
         table_remove(grid, idx)
         draw_noise_ore_patch(surface, seed, name, pos, radius, richness)
@@ -57,12 +56,14 @@ local function draw_grid_ore_patch(surface, seed, count, grid, name, radius, ric
 end
 
 
-local function generate_spawn_ore(surface, seed, direction)
+function Public.generate_spawn_ore(surface, seed, direction)
     surface = surface or game.surfaces[global.bb_surface_name]
     seed = seed or surface.map_gen_settings.seed
     direction = direction or defines.direction.south
 
+    local silo_distance = TerrainParams.silo_distance
     local dv = DirectionVectors[direction]
+    local spawn_ore = TerrainParams.spawn_ore
 
     -- This array holds indicies of chunks onto which we desire to
     -- generate ore patches. It is visually representing north spawn
@@ -74,6 +75,7 @@ local function generate_spawn_ore(surface, seed, direction)
     --     {-1, -1}, {1, -1}, {2, -1},
     -- }
     local grid = {}
+    local random = game.create_random_generator(seed)
 
     -- Calculate left_top position of a chunk. It will be used as origin
     -- for ore drawing. Reassigns new coordinates to the grid.
@@ -81,19 +83,92 @@ local function generate_spawn_ore(surface, seed, direction)
     --     grid[i][1] = grid[i][1] * 32 + math.random(-12, 12)
     --     grid[i][2] = grid[i][2] * 32 + math.random(-24, -1)
     -- end
-    for y = 2, 5, 1 do
+    local offset = 0.0
+    local od = silo_distance / 2
+    for y = 1, 3, 1 do
         for x = 0, y, 1 do
-            local noise = Noises.random({x = x, y = y}, seed)
-            table_insert(grid, {x = (dv.x * x + noise * dv.y) * 32, y = (dv.y * y - noise * dv.x) * 32})
-            table_insert(grid, {x = (dv.x * x + noise * dv.y) * 32, y = (dv.y * y - noise * dv.x) * 32})
+            offset = random(-0.4, 0.4)
+            -- LuaFormatter off
+            table_insert(grid, {
+                x = (dv.x * y - (x + offset) * dv.y) * od,
+                y = (dv.y * y + (x + offset) * dv.x) * od,
+            })
+            offset = random(-0.4, 0.4)
+            table_insert(grid, {
+                x = (dv.x * y + (x + offset) * dv.y) * od,
+                y = (dv.y * y - (x + offset) * dv.x) * od,
+            })
+            -- LuaFormatter on
         end
     end
 
     for name, props in pairs(spawn_ore) do
-        draw_grid_ore_patch(surface, seed, props.big_patches, grid, name, props.size, props.density)
-        draw_grid_ore_patch(surface, seed, props.small_patches, grid, name, props.size / 2, props.density)
+        draw_grid_ore_patch(surface, seed, random, props.big_patches, grid, name, props.size, props.density)
+        draw_grid_ore_patch(surface, seed, random, props.small_patches, grid, name, props.size / 2, props.density)
     end
 end
 
 
-return generate_spawn_ore
+local function _clear_resources(surface, area)
+    local resources = surface.find_entities_filtered {area = area, type = 'resource'}
+
+    local i = 0
+    for _, res in pairs(resources) do
+        if not res.valid then goto clear_resources_cont end
+        res.destroy()
+        i = i + 1
+
+        ::clear_resources_cont::
+    end
+
+    return i
+end
+
+
+function Public.clear_ore_in_main(surface, direction)
+    direction = direction or defines.direction.south
+
+    local sd = TerrainParams.silo_distance * 2
+    local dv = DirectionVectors[direction]
+
+    surface.request_to_generate_chunks({x = 0.0, y = 0.0}, sd / 32 + 1)
+    surface.force_generate_chunk_requests()
+
+    -- LuaFormatter off
+    local points = {
+        {x = (dv.x + dv.y)*sd, y = (dv.y - dv.x)*sd},
+        {x = (dv.x - dv.y)*sd, y = (dv.y + dv.x)*sd},
+        {x = (0.00 + dv.y)*sd, y = (0.00 - dv.x)*sd},
+        {x = (0.00 - dv.y)*sd, y = (0.00 + dv.x)*sd},
+    }
+    -- LuaFormatter on
+    local left_top = {x = 0.0, y = 0.0}
+    local right_bottom = {x = 0.0, y = 0.0}
+    for _, point in pairs(points) do
+        left_top.x = math_min(left_top.x, point.x)
+        left_top.y = math_min(left_top.y, point.y)
+        right_bottom.x = math_max(right_bottom.x, point.x)
+        right_bottom.y = math_max(right_bottom.y, point.y)
+    end
+    local limit = 20
+    local cnt = 0
+    repeat
+        -- Keep clearing resources until there is none.
+        -- Each cycle increases search area.
+        cnt = _clear_resources(surface, {left_top = left_top, right_bottom = right_bottom})
+        limit = limit - 1
+        left_top.x = left_top.x - 5
+        left_top.y = left_top.y - 5
+        right_bottom.x = right_bottom.x + 5
+        right_bottom.y = right_bottom.y + 5
+    until cnt == 0 or limit == 0
+
+    if limit == 0 then
+        log('Limit reached, some ores might be truncated in spawn area')
+        log('If this is a custom build, remove a call to clear_ore_in_main')
+        log('If this in a standard value, limit could be tweaked')
+    end
+end
+
+
+return Public

426724e92d5720ca525039d2c1b2399f810829d1
Atom735 <Atom735.ag47@gmail.com>
2021-10-16 18:40:37 +0300

    [TerrainNg] create spawn_circle

---
+++ terrain/spawn_circle.lua
@@ -0,0 +1,97 @@
+local TerrainParams = require 'terrain.table'
+local Noises = require 'utils.noises'
+
+local table_insert = table.insert
+local math_abs = math.abs
+local math_sqrt = math.sqrt
+local math_floor = math.floor
+
+local Public = {}
+
+function Public.contains(surface, seed, pos)
+    surface = surface or game.surfaces[global.bb_surface_name]
+    seed = seed or surface.map_gen_settings.seed
+
+    local x = math_floor(pos.x + 0.5)
+    local y = math_floor(pos.y + 0.5)
+    local r = math_sqrt(x ^ 2 + y ^ 2)
+
+    local outer_r = TerrainParams.outer_circle.radius
+    local outer_noise = TerrainParams.outer_circle.noise
+
+    if outer_noise >= 1.0 then
+        local noise = math_abs(Noises.spawn_circle_radius({x = x, y = y}, seed))
+        outer_r = outer_r - noise * outer_noise
+    end
+
+    return r <= outer_r
+end
+
+
+function Public.draw(surface, seed)
+    surface = surface or game.surfaces[global.bb_surface_name]
+    seed = seed or surface.map_gen_settings.seed
+
+    local outer_radius = TerrainParams.outer_circle.radius
+    local outer_noise = TerrainParams.outer_circle.noise
+    local inner_radius = TerrainParams.inner_circle.radius
+    local inner_sand = TerrainParams.inner_circle.sand
+    local fish_factor = TerrainParams.fish_factor
+
+    surface.request_to_generate_chunks({0, 0}, (outer_radius) / 32 + 1)
+    surface.force_generate_chunk_requests()
+
+    local tiles = {}
+    local entities = {}
+
+    for x = 0, outer_radius, 1 do
+        for y = 0, outer_radius, 1 do
+            -- LuaFormatter off
+            local pos_rb = {x = x+0.5, y = y+0.5}
+            local pos_rt = {x = x+0.5, y =-y-0.5}
+            local pos_lb = {x =-x-0.5, y = y+0.5}
+            local pos_lt = {x =-x-0.5, y =-y-0.5}
+            -- LuaFormatter on
+            local r = math_sqrt(x ^ 2 + y ^ 2)
+            local outer_r = outer_radius
+            local outer_r2 = outer_radius - outer_noise
+            local outer_r3 = inner_radius + outer_noise
+            if outer_noise >= 1.0 then
+                local noise = math_abs(Noises.spawn_circle_radius(pos_rb, seed))
+                outer_r = outer_r - noise * outer_noise
+                outer_r2 = outer_r - ((noise * outer_noise) * 2)
+                outer_r3 = inner_radius + ((noise * outer_noise) * 2)
+            end
+            if r <= outer_r then
+                local tile_name = 'deepwater'
+                if r >= outer_r2 or r <= outer_r3 then tile_name = 'water' end
+                if r < inner_radius then
+                    if r < inner_sand then
+                        tile_name = 'sand-1'
+                    else
+                        tile_name = 'refined-concrete'
+                    end
+                end
+                table_insert(tiles, {name = tile_name, position = pos_rb})
+                table_insert(tiles, {name = tile_name, position = pos_rt})
+                table_insert(tiles, {name = tile_name, position = pos_lb})
+                table_insert(tiles, {name = tile_name, position = pos_lt})
+                if tile_name == 'deepwater' then
+                    local fish_noise = math_abs(Noises.random(pos_rb, seed))
+                    if fish_noise > fish_factor then
+                        table_insert(entities, pos_rb)
+                        table_insert(entities, pos_rt)
+                        table_insert(entities, pos_lb)
+                        table_insert(entities, pos_lt)
+                    end
+                end
+            end
+        end
+    end
+    surface.set_tiles(tiles, true)
+
+    for i = 1, #entities, 1 do surface.create_entity({name = 'fish', position = entities[i]}) end
+end
+
+
+return Public
